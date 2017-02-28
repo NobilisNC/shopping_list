e3815d4d9e2984ff6ca12eb6c12cf5da1461450b
gagarine666 <youri.muller.97@gmail.com>
2017-02-28 21:45:37 +0100

    doc

--- application/models/ShopList_model.php
+++ application/models/ShopList_model.php
@@ -7,25 +7,35 @@ class ShopList_model extends CI_Model{
 
    /** @brief Gets the shop list for the specified user
     *
-    * @param $id_user - A specified user's id
+    * @param $user_id - A specified user's id
     *
     * @return The shop list
     */
-    public function getShops(int $id_user){
+    public function getShops(int $user_id){
         $this->db->from('shop');
         $this->db->join('user_shops','shop.id=user_shops.id_shop');
-        $this->db->where('user_shops.id_user',$id_user);
+        $this->db->where('user_shops.id_user',$user_id);
         $this->db->where('shop.etat','approved');
         $query = $this->db->get();
         return $query->result();
     }
 
+    /** @brief Gets all the shops
+     *
+     * @return The list of all the shops
+     */
     public function getAllShops(){
         $this->db->order_by('name', 'asc');
         $query = $this->db->get('shop');
         return $query->result();
     }
 
+    /** @brief Gets shop giving the first letters of its name
+     *
+     * @param $in - The beginning of a shop name
+     *
+     * @return The name and location of corresponding shops
+     */
     public function getShopIn(string $in){
         $this->db->select('name,location')
                  ->from('shop')
@@ -36,7 +46,12 @@ class ShopList_model extends CI_Model{
         return $query->result();
     }
 
-    public function addShopToUser($user_id,$shop_name){
+    /** @brief Adds a shop to a specified user's shop list
+     *
+     * @param $user_id - A specified user's id
+     * @param $shop_name - A specified shop name
+     */
+    public function addShopToUser($user_id, $shop_name){
 
         $shop_information = explode(" - ",$this->input->post('name_shop_to_add'));
         $name = $shop_information[0];
@@ -64,14 +79,23 @@ class ShopList_model extends CI_Model{
         }
     }
 
-    public function deleteShop(int $id_shop){
-        $this->db->where('id',$id_shop);
+    /** @brief Delete a shop with a specified id
+     *
+     * @param $shop_id - A specified shop id
+     */
+    public function deleteShop(int $shop_id){
+        $this->db->where('id',$shop_id);
         $this->db->delete('shop');
     }
 
-    public function deleteFromMyShops(int $id_shop,int $id_user){
-        $this->db->where('id_shop',$id_shop)
-        ->where('id_user',$id_user)
+    /** @brief Deletes a specified shop from a specified user's shop list
+    *
+    * @param $shop_id - A specified shop id
+    * @param $user_id
+    */
+    public function deleteFromMyShops(int $shop_id,int $user_id){
+        $this->db->where('id_shop',$shop_id)
+        ->where('id_user',$user_id)
         ->delete('user_shops');
     }
 }
