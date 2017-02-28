220b5637d1a8ceed1c417459ccb9f28cb63d1fe9
Troussier Julien <Troussier.julien@gmail.com>
2017-02-28 16:15:31 +0100

    Add sort

--- bdd_projet_tut.sql
+++ bdd_projet_tut.sql
@@ -70,7 +70,7 @@ CREATE TABLE `list_product` (
 CREATE TABLE `product` (
   `id` bigint(20) UNSIGNED NOT NULL,
   `name` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `expiration` tinyint UNSIGNED NOT NULL DEFAULT '0',
+  `coldness` tinyint UNSIGNED NOT NULL DEFAULT '0',
   `weight` float NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
