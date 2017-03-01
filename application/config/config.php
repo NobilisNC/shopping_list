b218bc5ef782201ea9182fa50437dab98aeb01fb
Tom HENOCH <tom.henoch@hotmail.fr>
2017-03-01 11:01:15 +0100

    caca

--- application/config/config.php
+++ application/config/config.php
@@ -25,7 +25,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 */
 
 
-$config['base_url'] = 'http://localhost/PTUT/shopping_list/';
+$config['base_url'] = 'http://localhost/~nobilis/ProjetTut/';
 /* Tom = $config['base_url'] = 'http://localhost/~nobilis/ProjetTut/';
  *
  */
