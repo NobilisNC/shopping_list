43816c0b8d69af004094590c256a19c5d8a6040b
gagarine666 <youri.muller.97@gmail.com>
2017-03-23 11:37:42 +0100

    doc API

--- application/controllers/API_smartphone.php
+++ application/controllers/API_smartphone.php
@@ -13,10 +13,10 @@
 
         /** @brief Creates a new useList from an existing list
         *
+        * @detail Verifies if the user is logged and if he owns the specified list.
+        *         If yes, creates a new useList from the specified list
         *
-        *
-        *
-        *
+        * @param $id - A specified list id
         */
         public function useList(int $id) {
           $response = array();
@@ -35,10 +35,24 @@
         }
 
 
+        /** @brief Deletes the specified useList
+        *
+        * @detail Calls deleteList($id) from UseList_model.
+        *
+        * @param $id - A specified useList id
+        */
         public function stopList(int $id) {
           $this->UseList_model->deleteList($id);
         }
 
+        /** @brief Checks a product as "taken" in the specified useList
+        *
+        * @detail Verifies if the user is logged, then calls check($id_product,
+        *         $id_list) from UseList_model.
+        *
+        * @param $id_list - A specified useList id
+        * @param $id_product - A specified product id
+        */
         public function checkProduct(int $id_list, int $id_product) {
           $reponse = array();
           if($this->session->userdata('logged_in') !== true )
@@ -51,6 +65,14 @@
           echo json_encode($response);
         }
 
+        /** @brief Unchecks a checked product in the specified useList
+        *
+        * @detail Verifies if the user is logged, then calls uncheck($id_product,
+        *         $id_list) from UseList_model.
+        *
+        * @param $id_list - A specified useList id
+        * @param $id_product - A specified product id
+        */
         public function uncheckProduct(int $id_list, int $id_product) {
           $reponse = array();
           if($this->session->userdata('logged_in') !== true )
@@ -63,6 +85,13 @@
           echo json_encode($response);
         }
 
+        /** @brief Displays a specified useList
+        *
+        * @detail Verifies if the user is logged and is an owner of the useList,
+        *         then calls getProducts($id) from UseList_model
+        *
+        * @param $id - A specified useList id
+        */
         public function getList(int $id) {
           $reponse = array();
           if($this->session->userdata('logged_in') !== true )
@@ -75,6 +104,14 @@
           echo json_encode($response);
         }
 
+        /** @brief Sorts the specified useList by weight
+        *
+        * @detail Verifies if the user is logged, then calls getProducts($id)
+        *         from UseList_model and compares the returned products weight.
+        *         Finally, displays the products from the heaviest to the lightest
+        *
+        * @param $id - A specified useList id
+        */
         public function sortWeight(int $id) {
           $reponse = array();
           if($this->session->userdata('logged_in') !== true )
@@ -94,6 +131,14 @@
           echo json_encode($response);
         }
 
+        /** @brief Sorts the specified useList by coldness
+        *
+        * @detail Verifies if the user is logged, then calls getProducts($id)
+        *         from UseList_model and compares the returned products coldness level.
+        *         Finally, displays the products from the less cold to the coldest
+        *
+        * @param $id - A specified useList id
+        */
         public function sortColdness(int $id) {
           $reponse = array();
           if($this->session->userdata('logged_in') !== true )
