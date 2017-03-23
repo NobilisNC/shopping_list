151069ba702eb168bfdcc88693975ad6a70a2ab6
Tom HENOCH <tom.henoch@hotmail.fr>
2017-03-23 21:26:31 +0100

    API + clean de la version ou les shop etait relier a user

--- bdd_projet_tut.sql
+++ bdd_projet_tut.sql
@@ -99,14 +99,6 @@ CREATE TABLE `shop` (
   `location` varchar(30) COLLATE utf8mb4_unicode_ci NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
---
--- Structure de la table `user_shops`
---
-
-CREATE TABLE `user_shops` (
-  `id_shop` bigint(20) UNSIGNED NOT NULL,
-  `id_user` bigint(20) UNSIGNED NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Indexes for dumped tables
@@ -154,13 +146,6 @@ ALTER TABLE `user`
 ALTER TABLE `shop`
   ADD PRIMARY KEY (`id`);
 
---
--- Index pour la table `user_shops`
---
-ALTER TABLE `user_shops`
-  ADD PRIMARY KEY (`id_shop`,`id_user`),
-  ADD KEY `id_shop` (`id_shop`),
-  ADD KEY `id_user` (`id_user`);
 
 --
 -- AUTO_INCREMENT for dumped tables
@@ -190,7 +175,6 @@ ALTER TABLE `shop`
   /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
   /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 
-ALTER TABLE `shop` ADD `etat` ENUM('submited','approved') NOT NULL AFTER `location`;
 ALTER TABLE `user`  ADD `isAdmin` BOOLEAN NOT NULL DEFAULT FALSE  AFTER `fname`;
 
 --
@@ -221,12 +205,6 @@ ALTER TABLE `list_product`
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 
---
--- Contraintes pour la table `user_shops`
---
-ALTER TABLE `user_shops`
-  ADD CONSTRAINT `FK_ID_SHOP` FOREIGN KEY (`id_shop`) REFERENCES `shop` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
-  ADD CONSTRAINT `FK_ID_USER` FOREIGN KEY (`id_user`) REFERENCES `user` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
 -- Table structure for table `use_list`
